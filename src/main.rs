4cce724f76ff6cf18c4ffe6a4003d0c872aadcd8
paul wisehart <paul@oldcode.org>
2019-08-12 18:22:27 -0400

    asd

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 fn main() {
-	println!("hello rust");
 	let x = 5 + /* 90 + */ 5;
+
+	let logical: bool = true;
+	let a_float: f64 = 1.0;
+	let an_int = 5i32;
+
+
 	println!("x = {}", x);
+	println!("logical:{} flt:{} int:{}", logical, a_float, an_int);
 }
 
