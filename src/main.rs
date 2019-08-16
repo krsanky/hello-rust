806490ca80f9e9e1c7380825e9a6cfb6cc835e8a
paul wisehart <paul@oldcode.org>
2019-08-16 11:10:08 -0400

    asd

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
 extern crate rand;
 
-use std::io;
-//use std::cmp::Ordering;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
-	let secret_number = rand::thread_rng().gen_range(1,101);
-	println!("secret num:{}", secret_number);
-	println!("Guess the number bro!");
-	println!("input number:");
-	let mut guess = String::new();
-	io::stdin().read_line(&mut guess)
-		.expect("failed to read");
-	println!("you guessed {}", guess);
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("secret num:{}", secret_number);
+    println!("Guess the number bro!");
+    println!("input number:");
+    let mut guess = String::new();
+    io::stdin().read_line(&mut guess).expect("failed to read");
+    println!("you guessed {}", guess);
+
+    // shadowing ?
+    let guess: u32 = guess.trim().parse().expect("error converting guess to u32");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("too small"),
+        Ordering::Greater => println!("too big"),
+        Ordering::Equal => println!("you win!"),
+    }
 }
