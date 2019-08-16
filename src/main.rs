4e5b9d80f669ce592bfdf94a910d8d90d538bf0f
paul wisehart <paul@oldcode.org>
2019-08-16 15:24:37 -0400

    asd

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,24 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1, 101);
     println!("secret num:{}", secret_number);
     println!("Guess the number bro!");
-    println!("input number:");
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("failed to read");
-    println!("you guessed {}", guess);
 
-    // shadowing ?
-    let guess: u32 = guess.trim().parse().expect("error converting guess to u32");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("too small"),
-        Ordering::Greater => println!("too big"),
-        Ordering::Equal => println!("you win!"),
-    }
+	loop {
+		println!("input number:");
+		let mut guess = String::new();
+		io::stdin().read_line(&mut guess).expect("failed to read");
+		println!("you guessed {}", guess);
+
+		// shadowing ?
+		let guess: u32 = guess.trim().parse().expect("error converting guess to u32");
+
+		match guess.cmp(&secret_number) {
+			Ordering::Less => println!("too small"),
+			Ordering::Greater => println!("too big"),
+			Ordering::Equal => {
+				println!("you win!");
+				break;
+			}
+		}
+	}
 }
