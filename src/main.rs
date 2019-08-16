a515b78ef606780002a687a6831a67325051a6e1
paul wisehart <paul@oldcode.org>
2019-08-16 10:53:57 -0400

    asd

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use std::io;
 use rand::Rng;
 
 fn main() {
+	let secret_number = rand::thread_rng().gen_range(1,101);
+	println!("secret num:{}", secret_number);
 	println!("Guess the number bro!");
 	println!("input number:");
 	let mut guess = String::new();
